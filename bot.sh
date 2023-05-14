0a0e11d3faf7f4d3f735722f2168e7870fbf7cd1
mstgst <mostlygostly@gmail.com>
2023-05-14 11:52:22 -0400

    test

--- bot.sh
+++ bot.sh
@@ -4,31 +4,31 @@ os="$(uname -s)"
 echo "commit-bot detects OS: $os"
 
 case $os in
-	
-	    Darwin)
-		            cd "`dirname $0`" || exit 1
-			            ;;
-				    
-				        Linux)
-						        cd "$(dirname "$(readlink -f "$0")")" || exit 1
-							        ;;
-								
-								    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-									            echo "OS unsupported (submit an issue on GitHub!)"
-										            ;;
-											    
-											        *)
-													        echo "OS unsupported (submit an issue on GitHub!)"
-														        ;;
-													esac
-													
-													echo "$info" >> output.txt
-													echo "$info"
-													echo
-													
-													# Ship it
-													git add output.txt
-													git commit -m "$info"
-													git push origin main # or "master" on old setups
-													
-													cd --
+
+Darwin)
+	cd "$(dirname $0)" || exit 1
+	;;
+
+Linux)
+	cd "$(dirname "$(readlink -f "$0")")" || exit 1
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	echo "OS unsupported (submit an issue on GitHub!)"
+	;;
+
+*)
+	echo "OS unsupported (submit an issue on GitHub!)"
+	;;
+esac
+
+echo "$info" >>output.txt
+echo "$info"
+echo
+
+# Ship it
+git add output.txt
+git commit -m "$info"
+git push origin main # or "master" on old setups
+
+cd --
